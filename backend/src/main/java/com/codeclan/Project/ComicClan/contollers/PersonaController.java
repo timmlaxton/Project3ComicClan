f929adcf044069063920d5594bd1ef4ee58b03fb
Shaun Kennedy <shaunhk@hotmail.com>
2019-08-09 18:42:34 +0100

    api pathways more intuitive

--- backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PersonaController.java
+++ backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PersonaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/personas")
+@RequestMapping(value = "api/characters")
 public class PersonaController {
     @Autowired
     PersonaRepository personaRepository;
--- backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PublisherController.java
+++ backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PublisherController.java
@@ -1,9 +1,13 @@
 package com.codeclan.Project.ComicClan.contollers;
 
+import com.codeclan.Project.ComicClan.models.Publisher;
 import com.codeclan.Project.ComicClan.repositories.publishers.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/publishers")
@@ -11,5 +15,8 @@ public class PublisherController {
 
     @Autowired
     PublisherRepository publisherRepository;
+
+    @GetMapping
+    public List<Publisher> getAllPublishers() { return publisherRepository.findAll();}
 
 }
