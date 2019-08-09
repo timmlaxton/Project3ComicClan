0a133d1ee8cf719d247c99333f9c8b06bf7c5433
Shaun Kennedy <shaunhk@hotmail.com>
2019-08-09 17:11:01 +0100

    frontend and backend

--- backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PersonaController.java
+++ backend/src/main/java/com/codeclan/Project/ComicClan/contollers/PersonaController.java
@@ -1,14 +1,23 @@
 package com.codeclan.Project.ComicClan.contollers;
 
 
+import com.codeclan.Project.ComicClan.models.Persona;
 import com.codeclan.Project.ComicClan.repositories.personas.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/personae")
+@RequestMapping(value = "/personas")
 public class PersonaController {
     @Autowired
     PersonaRepository personaRepository;
+
+    @GetMapping
+    public List<Persona> getAllPersonas(){
+        return personaRepository.findAll();
+    }
 }
