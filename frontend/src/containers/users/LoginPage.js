53d3b000e8f751529ce6899a3d80658b0acc3b21
Laura Sheach <laura_bubble_git@yahoo.com>
2019-08-10 19:21:00 +0100

    Login details now in middle of page

--- frontend/src/containers/users/LoginPage.js
+++ frontend/src/containers/users/LoginPage.js
@@ -4,13 +4,18 @@ import {Link} from 'react-router-dom';
 class LoginPage extends Component {
 
 
-
   render(){
     return (
       <div className="login">
-        <h3>Welcome to Comic Clan!</h3>
+        <h1>Welcome to Comic Clan!</h1>
+
+          <select name="user">
+            <option value="" disabled>Please select your user name:</option>
+          </select>
+          <br />
+        <button type="submit">Enter</button>
       </div>
-    )
+    );
   }
 
 }
