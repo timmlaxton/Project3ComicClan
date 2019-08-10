1492ce8ce6101a8fa55276888e325daa4305293d
Laura Sheach <laura_bubble_git@yahoo.com>
2019-08-10 13:55:51 +0100

    Basic comic component setup

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -4,13 +4,21 @@ import NavBar from '../NavBar.js';
 
 class MainContainer extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      comics: [],
+      personas: [],
+      publishers: [],
+      users: [],
+      reviews: []
+    }
+  }
+
   render(){
     return (
     <div>
       <NavBar />
-
-    
-
     </div>
   )
   }
