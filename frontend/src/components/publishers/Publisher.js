cddaacfd4da47be198764b42fded1c60781c48f1
Tim <timmlaxton@gmail.com>
2019-08-14 09:40:11 +0100

    changes persona description font size and moved details box down

--- frontend/src/components/publishers/Publisher.js
+++ frontend/src/components/publishers/Publisher.js
@@ -9,8 +9,8 @@ const Publisher = (props) => {
 
   return (
     <React.Fragment>
-    
-      <img src={props.publisher.logo} Transformation height="300" width="300" crop="fill" effect="sepia" radius="20" />
+
+      <img src={props.publisher.logo} Transformation height="200" width="200" crop="fill" effect="sepia" radius="20" />
     </React.Fragment>
   )
 
